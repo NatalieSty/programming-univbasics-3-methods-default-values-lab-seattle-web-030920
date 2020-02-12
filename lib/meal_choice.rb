c193b0e94fd616199332743b063ecaf0d5327e6a
Natalie So <nataliesotszyui@gmail.com>
2020-02-12 21:39:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 
-def meal_choice()
+def meal_choice(protein, veg1, veg2)
   puts "What a nutritious meal!"
-  puts 
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
